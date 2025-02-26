c9f6ff4f64043e5f4fa63685086302fb52aeb32c
Dan <contact@warpforged-creations.co.uk>
2025-02-26 20:31:05 +0000

    Fix logo path for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import icon from "astro-icon"; // Import astro-icon
 export default defineConfig({
   output: "static", // Required for GitHub Pages
   site: "https://www.warpforgedcreations.co.uk", // ⚠️ Replace with your actual GitHub Pages URL
+  base: "/",
   build: {
     assets: 'app_assets',
   },

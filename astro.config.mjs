0350fd6ad7ca5fee66c21ab954f0d95b06123532
Dan <contact@warpforged-creations.co.uk>
2025-02-26 11:18:32 +0000

    astro config, changed for assets under app_assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import icon from "astro-icon"; // Import astro-icon
 
 export default defineConfig({
   output: "static", // Required for GitHub Pages
+  site: "https://Dan458.github.io/", // ⚠️ Replace with your actual GitHub Pages URL
   base: "/Warp-Forged-Creations/", // ⚠️ Replace with your GitHub repo name
-  site: "https://Dan458.github.io/Warp-Forged-Creations/", // ⚠️ Replace with your actual GitHub Pages URL
+  build: {
+    assets: 'app_assets',
+  },
   integrations: [
     icon({
       sets: ["fa6-brands"], // Add FontAwesome 6 brands set
